e40fcbc77521868d736ae70dec252d0670559307
Francisco Berti <francisco.berti@zarpsystem.com.br>
2024-07-30 10:53:53 -0300

    Adicionando navegação em rotas nomeadas, definindo constantes para essas rotas, refactor

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,3 +1,6 @@
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
 import '../models/category.dart';
 import 'package:flutter/material.dart';
 
@@ -6,23 +9,41 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem({super.key, required this.category});
 
+  void _selectCategory(BuildContext context) {
+    // Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+    //   return CategoriesMealsScreen(
+    //     category: category,
+    //   );
+    // }));
+
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.5),
-              category.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.5),
+                category.color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/utils/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,18 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
         colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.pink,
           secondary: Colors.amber,
+          background: Colors.white,
         ),
         fontFamily: 'Raleway',
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-          titleLarge: const TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          )
-        ),
+                titleLarge: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
         appBarTheme: const AppBarTheme(
           titleTextStyle: TextStyle(
             fontSize: 20,
@@ -30,9 +32,13 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.pink,
           foregroundColor: Colors.white,
+          centerTitle: true,
         ),
       ),
-      home: const CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME : (context) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS : (context) => CategoriesMealsScreen(),
+      },
     );
   }
 }
